6662b14e93e1ca000a3ecf8c37f035d7d9142287
Md Risul Karim <risulkarimx1@gmail.com>
2021-01-06 19:57:19 +0100

    Added events and event handler pattern interface

---
+++ src/Actio.Common/Events/ActivityCreated.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Actio.Common.Events
+{
+    public class ActivityCreated : IAuthenticatedEvent
+    {
+        public Guid Id { get; }
+        public Guid UserId { get; }
+        public string Category { get; }
+        public string Name { get; }
+        public string Description { get; }
+        public DateTime CreatedTime { get; }
+
+        protected ActivityCreated()
+        {
+            
+        }
+
+        public ActivityCreated(Guid id, Guid userId, string category, string name, string description, DateTime createdTime)
+        {
+            Id = id;
+            UserId = userId;
+            Category = category;
+            Name = name;
+            Description = description;
+            CreatedTime = createdTime;
+        }
+    }
+}
---
+++ src/Actio.Common/Events/IEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Actio.Common.Events
+{
+    public interface IEventHandler <in T> where T: IEvent
+    {
+        Task HandleAsync(T @event);
+    }
+    // Marker inteface
+    // For each of the commands we need to have an event
+    public interface IEvent
+    {
+    }
+}
